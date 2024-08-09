dc3a0b6d80ea922a16fa29ebba4ea084ac48e965
lsy969999 <lsy969999@gmail.com>
2024-08-09 18:14:22 +0900

    infinite test

--- src/pages/test/TestGesture.tsx
+++ src/pages/test/TestGesture.tsx
@@ -4,25 +4,27 @@ import { useState } from "react"
 
 const TestGesture = () => {
     const [{x, y}, api] = useSpring(() => ({x: 0, y: 0}))
-    const [trsX, setTrsX] = useState(-40)
     const [w, setW] = useState(0)
-    
 
     const bind = useGesture({
         onDrag: ({down, movement:[mx, my]}) => {
+            const width = document.querySelector('body')!.clientWidth 
+            if (w >= width || w <= -width) {
+                setW(0)
+                x.set(0)
+                return;
+            }
             api.start({x: down ? mx + w : w, y: down ? my : 0})
             if(!down) {
-                console.log('release')
+                // console.log('release')
                 if (mx > 50) {
-                    console.log('move left')
-                    const width = document.querySelector('body')!.clientWidth 
-                    
+                    // console.log('move left')
                     setW(width + w)
                     api.start({ x: width + w });
+                    
                 }
                 if (mx < -50) {
-                    console.log('move right')
-                    const width = document.querySelector('body')!.clientWidth 
+                    // console.log('move right')
                     setW(w - width)
                     api.start({ x: w - width });
                 }
